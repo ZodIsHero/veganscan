3da1b84421ebd1fa1172454ae998a8577c4ceea8
ZodIsHero <CruelDugal@gmail.com>
2023-05-28 02:46:59 +0500

    Initialize

---
+++ src/main/java/com/veganprojects/veganscan/VeganscanApplication.java
@@ -0,0 +1,13 @@
+package com.veganprojects.veganscan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VeganscanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VeganscanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/veganprojects/veganscan/VeganscanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veganprojects.veganscan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VeganscanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
